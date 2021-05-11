54f05d5eaed6db23c8c2f926f552c7e307be6abf
Michael Estes <michael@fastlight.com>
2021-05-11 09:58:48 -0400

    Updated

--- COMMON/HelloWorld.java
+++ COMMON/HelloWorld.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class HelloWorld {
 
+    public static void runme() {
+        System.out.println("Hello World!");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
