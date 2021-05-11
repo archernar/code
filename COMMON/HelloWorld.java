b97ff466a606939164550b2c9d29e12d5151e79d
Michael Estes <michael@fastlight.com>
2021-05-11 10:19:45 -0400

    Updated

--- COMMON/HelloWorld.java
+++ COMMON/HelloWorld.java
@@ -7,6 +7,15 @@ public class HelloWorld {
     }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 
 }
