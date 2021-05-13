86cd45ba32a2cdafc13040ce613b7d0731794b14
Michael Estes <michael@fastlight.com>
2021-05-13 08:11:59 -0400

    Updated

---
+++ OTHER/HelloWorld.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class HelloWorld {
+
+    public static void runme() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+        System.out.println("Hello World!!.........!!");
+    }
+
+}
+
+
